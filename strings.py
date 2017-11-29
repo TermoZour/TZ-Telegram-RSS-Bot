bfc59a65894e119e38fec78764e5aec86e895e78
TermoZour <stefanpruneanu@gmail.com>
2017-11-29 22:43:25 +0200

    added new strings for help message

--- strings.py
+++ strings.py
@@ -4,8 +4,7 @@ errorNoFile = "Either file is missing or is not readable. Creating."
 errorCommand = "Unknown command."
 errorAdmin = "You must be an admin to issue this command"
 
-stringHelp = "Type /help for a list of commands."
-
+stringHelp = "Hello, I am TermoZour's RSS Bot. I can give you updates to RSS links! Type /help for a list of commands."
 
 stringURLadded = "Added URL to subscription"
 stringURLremoved = "Removed URL from subscription"
@@ -15,3 +14,7 @@ stringURLalreadyAdded = "This URL has already been added"
 stringURLalreadyRemoved = "You haven't subscribed to this URL yet"
 
 help_message = "\n/help - shows this message"
+help_add = "\n/add <link> - add the link to the subscriptions"
+help_remove = "\n/remove <link> - removes the link from the subscriptions"
+help_url = "\n/url <link> - shows the link's data and the last entry, for testing purposes i suppose"
+help_list = "\n/list - shows the list of links you currently subscribed to in that particular chat"
